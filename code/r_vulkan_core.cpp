67a0e00709ab5e2a4cbe8b0f53ccc6873937a427
TheSleepster <ibjal20@outlook.com>
2026-02-15 13:54:50 -0600

    just doing anything to push this as the new head

--- code/r_vulkan_core.cpp
+++ code/r_vulkan_core.cpp
@@ -34,6 +34,7 @@
 // VULKAN UTILITIES
 /////////////////////////
 
+#if 0
 VKAPI_ATTR VkBool32 VKAPI_CALL
 Vk_debug_log_callback(VkDebugUtilsMessageSeverityFlagBitsEXT      message_severity,
                       VkDebugUtilsMessageTypeFlagsEXT             message_type,
@@ -65,6 +66,7 @@ Vk_debug_log_callback(VkDebugUtilsMessageSeverityFlagBitsEXT      message_severi
     }
     return VK_FALSE;
 }
+#endif
 
 const char* 
 r_vulkan_result_string(VkResult result, bool8 get_extended) 
