7df6e5277d846e5cbc836a56f19d674acc7987bc
Lewis <ibjal20@outlook.com>
2026-08-27 20:52:06 -0500

    linking game actions up to the event system worked well

--- code/sandbox/input.cpp
+++ code/sandbox/input.cpp
@@ -31,15 +31,15 @@ enum game_action_binding_type_t
 
 struct game_action_binding_t
 {
-    u32 binding_id;
-    u32 binding_type;
+    s32 bindingID;
+    s32 binding_type;
 };
 
 struct game_action_mapping_t
 {
     game_action_binding_t bindings[MAX_GAME_ACTION_BINDINGS];
     s32                   binding_count;
-    u32                   controller_type;
+    s32                   controller_type;
 };
 
 struct game_action_t
@@ -49,10 +49,9 @@ struct game_action_t
     s32                   mapping_count;
 
     string_t              name;
-    // TODO(Sleepster): Maybe we want to cache the values related to the input axis and such?
-    // vec2_t  axis2D_value;
-    // float32 axis1D_value;
-    // s32     button_flags;
+    vec2_t                axis2D_value;
+    float32               axis1D_value;
+    s32                   button_flags;
 };
 
 
@@ -105,8 +104,7 @@ struct keyboard_controller_data_t
 struct gamepad_controller_data_t
 {
     SDL_Gamepad    *gamepad_data;
-    SDL_Joystick   *stick_data;
-    u32             gamepad_id;
+    u32             gamepadID;
 
     bool8           has_rumble;
     s32             rumble_value;
@@ -153,12 +151,11 @@ enum input_axis_t
 // NOTE(Sleepster): -1 on any of the ID's means that it is "Invalid" 
 struct input_event_t
 {
-    u32      type;
-    u32      input_type;
+    s32      type;
+    s32      input_type;
     bool32   consumed;
 
     s32      inputID;      // key / gamepad button
-    s32      scancode;     // SDL_Scancode
     s32      controllerID; // owner controller
     u64      timestampMS;
 
@@ -181,7 +178,7 @@ struct input_controller_t
     };
 };
 
-// TODO(Sleepster): input_manager_t 
+// TODO(Sleepster): event_handler_t 
 struct input_event_handler_t 
 {
     s32 event_count;
@@ -249,6 +246,25 @@ find_first_keyboard_controller(input_event_handler_t *event_handler, s32 *index_
             result = controller;
             if(index_out) *index_out = controller_index; 
 
+            break;
+        }
+    }
+
+    return(result);
+}
+
+input_controller_t*
+find_controller_of_type(input_event_handler_t *event_handler, s32 type)
+{
+    input_controller_t *result = null;
+    for(u32 controller_index = 0;
+        controller_index < MAX_INPUT_CONTROLLERS;
+        ++controller_index)
+    {
+        input_controller_t *controller = event_handler->controllers + controller_index;
+        if(controller->type == type)
+        {
+            result = controller;
             break;
         }
     }
@@ -325,6 +341,239 @@ SDL_axis_to_input_axis(u32 SDL_axis)
     return(result);
 }
 
+// game actions
+game_action_t*
+s_im_game_action_create(input_event_handler_t     *event_handler, 
+                        string_t                   action_name, 
+                        game_action_mapping_type_t mapping_type)
+{
+    game_action_t *result = null;
+
+    game_action_t new_action = {
+        .action_binding_type = mapping_type,
+        .name                = action_name
+    };
+    c_dynarray_add(&event_handler->game_actions, &new_action);
+    result = &event_handler->game_actions[event_handler->game_actions.used - 1];
+
+    return(result);
+}
+
+void
+s_im_game_action_add_mapping(game_action_t *action, game_action_mapping_t *mapping)
+{
+    Expect((action->mapping_count + 1) <= MAX_GAME_ACTION_MAPPINGS, "Attempted to add more mappings to a game_action than allowed... the max is 4...\n");
+    action->mappings[action->mapping_count++] = *mapping;
+}
+
+void
+s_im_game_action_reset_mappings(game_action_t *action)
+{
+    ZeroMemory(action->mappings, sizeof(game_action_mapping_t) * MAX_GAME_ACTION_MAPPINGS);
+    action->mapping_count = 0;
+}
+
+s32
+s_im_get_button_binding_state(input_controller_t *controller, game_action_binding_t *binding)
+{
+    s32 result = 0;
+    for(s32 event_index = 0;
+        event_index < controller->event_count;
+        ++event_index)
+    {
+        input_event_t *event = controller->events + event_index; 
+        if((event->input_type == controller->type && !event->consumed) &&
+           (binding->bindingID == event->inputID))
+        {
+            switch(event->type)
+            {
+                case INPUT_EVENT_TYPE_PRESSED:
+                {
+                    result |= INPUT_MANAGER_ACTION_BUTTON_FLAG_PRESSED;
+                    result |= (result & ~INPUT_MANAGER_ACTION_BUTTON_FLAG_RELEASED);
+                }break;
+                case INPUT_EVENT_TYPE_DOWN:
+                {
+                    result |= INPUT_MANAGER_ACTION_BUTTON_FLAG_DOWN;
+                    result |= (result & ~INPUT_MANAGER_ACTION_BUTTON_FLAG_PRESSED);
+                }break;
+                case INPUT_EVENT_TYPE_RELEASED:
+                {
+                    result |= INPUT_MANAGER_ACTION_BUTTON_FLAG_RELEASED;
+                    result |= (result & ~INPUT_MANAGER_ACTION_BUTTON_FLAG_PRESSED);
+                    result |= (result & ~INPUT_MANAGER_ACTION_BUTTON_FLAG_DOWN);
+                }break;
+            }
+        }
+    }
+
+    return(result);
+}
+
+s16
+s_im_get_axis_value(input_controller_t *controller, game_action_binding_t *binding)
+{
+    s16 result = 0;
+    for(s32 event_index = 0;
+        event_index < controller->event_count;
+        ++event_index)
+    {
+        input_event_t *event = controller->events + event_index; 
+        if((event->input_type == controller->type && !event->consumed) &&
+           (event->type == INPUT_EVENT_TYPE_AXIS_MOVED) && event->inputID == binding->bindingID)
+        {
+            result = event->axis_value;
+        }
+    }
+
+    return(result);
+}
+
+void
+s_im_game_action_process_button_state(input_controller_t *controller, game_action_t *action)
+{
+    game_action_mapping_t *mapping = null;
+    for(s32 mapping_index = 0;
+        mapping_index < action->mapping_count;
+        ++mapping_index)
+    {
+        game_action_mapping_t *found = action->mappings + mapping_index;
+        if(found->controller_type == controller->type)
+        {
+            mapping = found;
+            break;
+        }
+    }
+
+    if(mapping)
+    {
+        s32 key_state = s_im_get_button_binding_state(controller, &mapping->bindings[0]);
+        action->button_flags = key_state;
+    }
+}
+
+void
+s_im_game_action_process_axis1D_state(input_controller_t *controller, game_action_t *action)
+{
+    float32 axis_value = 0.0f;
+
+    game_action_mapping_t *mapping = null;
+    for(s32 mapping_index = 0;
+        mapping_index < action->mapping_count;
+        ++mapping_index)
+    {
+        game_action_mapping_t *found = action->mappings + mapping_index;
+        if(found->controller_type == controller->type)
+        {
+            mapping = found;
+            break;
+        }
+    }
+
+    if(mapping)
+    {
+        switch(mapping->controller_type)
+        {
+            case INPUT_CONTROLLER_TYPE_KEYBOARD:
+            {
+                s32 button0 = s_im_get_button_binding_state(controller, &mapping->bindings[0]);
+                s32 button1 = s_im_get_button_binding_state(controller, &mapping->bindings[1]);
+                if(button0 & INPUT_MANAGER_ACTION_BUTTON_FLAG_DOWN)
+                {
+                    axis_value += 1.0f;
+                }
+
+                if(button1 & INPUT_MANAGER_ACTION_BUTTON_FLAG_DOWN)
+                {
+                    axis_value += -1.0f;
+                }
+
+                action->axis1D_value = axis_value;
+            }break;
+            case INPUT_CONTROLLER_TYPE_GAMEPAD:
+            {
+                action->axis1D_value = s_im_get_axis_value(controller, &mapping->bindings[0]);
+            }break;
+        }
+    }
+}
+
+void
+s_im_game_action_process_axis2D_state(input_controller_t *controller, game_action_t *action)
+{
+    vec2_t axis_value = vec2_zero();
+
+    game_action_mapping_t *mapping = null;
+    for(s32 mapping_index = 0;
+        mapping_index < action->mapping_count;
+        ++mapping_index)
+    {
+        game_action_mapping_t *found = action->mappings + mapping_index;
+        if(found->controller_type == controller->type)
+        {
+            mapping = found;
+            break;
+        }
+    }
+
+    if(mapping)
+    {
+        switch(mapping->controller_type)
+        {
+            case INPUT_CONTROLLER_TYPE_KEYBOARD:
+            {
+                s32 button0 = s_im_get_button_binding_state(controller, &mapping->bindings[0]);
+                s32 button1 = s_im_get_button_binding_state(controller, &mapping->bindings[1]);
+                s32 button2 = s_im_get_button_binding_state(controller, &mapping->bindings[2]);
+                s32 button3 = s_im_get_button_binding_state(controller, &mapping->bindings[3]);
+                if(button0 & (INPUT_MANAGER_ACTION_BUTTON_FLAG_PRESSED|INPUT_MANAGER_ACTION_BUTTON_FLAG_DOWN))
+                {
+                    axis_value.y += 1.0;
+                }
+                if(button1 & (INPUT_MANAGER_ACTION_BUTTON_FLAG_PRESSED|INPUT_MANAGER_ACTION_BUTTON_FLAG_DOWN))
+                {
+                    axis_value.y += -1.0;
+                }
+                if(button2 & (INPUT_MANAGER_ACTION_BUTTON_FLAG_PRESSED|INPUT_MANAGER_ACTION_BUTTON_FLAG_DOWN))
+                {
+                    axis_value.x += -1.0;
+                }
+                if(button3 & (INPUT_MANAGER_ACTION_BUTTON_FLAG_PRESSED|INPUT_MANAGER_ACTION_BUTTON_FLAG_DOWN))
+                {
+                    axis_value.x += 1.0;
+                }
+            }break;
+            case INPUT_CONTROLLER_TYPE_GAMEPAD:
+            {
+                s16 Yaxis = s_im_get_axis_value(controller, &mapping->bindings[0]);
+                s16 Xaxis = s_im_get_axis_value(controller, &mapping->bindings[1]);
+
+                float32 Yaxis_normalized = Yaxis < 0 ? (float32)Yaxis / 32768.0f : (float32)Yaxis / 32767.0f;
+                float32 Xaxis_normalized = Xaxis < 0 ? (float32)Xaxis / 32768.0f : (float32)Xaxis / 32767.0f;
+
+                // NOTE(Sleepster): SDL reports up as negative, down as positive. Flip so +Y == up.
+                axis_value = {Xaxis_normalized, -Yaxis_normalized};
+
+                float32 magnitude = vec2_length(axis_value);
+                if(magnitude > controller->gamepad.stick_deadzone)
+                {
+                    float32 scaled_magnitude = (magnitude - controller->gamepad.stick_deadzone) / (1.0f - controller->gamepad.stick_deadzone);
+                    scaled_magnitude = Clamp(scaled_magnitude, 0.0f, 1.0f);
+
+                    axis_value = vec2_multiply(axis_value, vec2_create(scaled_magnitude / magnitude));
+                    axis_value = vec2_normalize(axis_value);
+                }
+                else
+                {
+                    axis_value = vec2_zero();
+                }
+            }break;
+        }
+    }
+
+    action->axis2D_value = axis_value;
+}
+
 int
 main(void)
 {   
@@ -339,6 +588,42 @@ main(void)
 
     SDL_ShowWindow(window);
     SDL_StartTextInput(window);
+
+    // NOTE(Sleepster): Movement 
+    game_action_t *movement_action = s_im_game_action_create(&event_handler, STR("character move"), INPUT_MANAGER_GAME_ACTION_MAPPING_TYPE_AXIS2D);
+
+    game_action_mapping_t keyboard_movement_mapping = (game_action_mapping_t) {
+        .bindings = {{SDL_SCANCODE_W}, {SDL_SCANCODE_S}, {SDL_SCANCODE_A}, {SDL_SCANCODE_D}},
+        .binding_count = 4,
+        .controller_type = INPUT_CONTROLLER_TYPE_KEYBOARD 
+    };
+    s_im_game_action_add_mapping(movement_action, &keyboard_movement_mapping);
+
+    game_action_mapping_t controller_movement_mapping = (game_action_mapping_t) {
+        .bindings = {
+            {SDL_GAMEPAD_AXIS_LEFTY, INPUT_MANAGER_BINDING_TYPE_JOYSTICK}, 
+            {SDL_GAMEPAD_AXIS_LEFTX, INPUT_MANAGER_BINDING_TYPE_JOYSTICK}
+        },
+        .binding_count   = 2,
+        .controller_type = INPUT_CONTROLLER_TYPE_GAMEPAD 
+    };
+    s_im_game_action_add_mapping(movement_action, &controller_movement_mapping);
+
+    // NOTE(Sleepster): Jump 
+    game_action_t *jump_action = s_im_game_action_create(&event_handler, STR("Jump"), INPUT_MANAGER_GAME_ACTION_MAPPING_TYPE_BUTTON);
+    game_action_mapping_t keyboard_jump_mapping = (game_action_mapping_t) {
+        .bindings = {{SDL_SCANCODE_SPACE}},
+        .binding_count = 1,
+        .controller_type = INPUT_CONTROLLER_TYPE_KEYBOARD
+    };
+    s_im_game_action_add_mapping(jump_action, &keyboard_jump_mapping);
+
+    game_action_mapping_t gamepad_jump_mapping = (game_action_mapping_t) {
+        .bindings = {{SDL_GAMEPAD_BUTTON_SOUTH}},
+        .binding_count = 1,
+        .controller_type = INPUT_CONTROLLER_TYPE_GAMEPAD
+    };
+    s_im_game_action_add_mapping(jump_action, &gamepad_jump_mapping);
 
     u64 perf_count_freq = SDL_GetPerformanceFrequency();
     u64 last_tsc        = SDL_GetPerformanceCounter();
@@ -377,14 +662,11 @@ main(void)
                 case SDL_EVENT_KEY_DOWN:
                 case SDL_EVENT_KEY_UP:
                 {
-                    u32 keyID = event.key.key;
-
                     input_event_t input_event = {};
                     input_event.input_type    = INPUT_CONTROLLER_TYPE_KEYBOARD;
-                    input_event.inputID       = keyID;
+                    input_event.inputID       = event.key.scancode;
                     input_event.controllerID  = event.key.which;
                     input_event.timestampMS   = SDL_GetTicks();
-                    input_event.scancode      = event.key.scancode;
 
                     bool8 pressed  =  event.key.down;
                     bool8 down     =  event.key.repeat;
@@ -496,7 +778,7 @@ main(void)
                     new_controller.ID               = event.gdevice.which;
                     new_controller.controller_index = event_handler.connected_controller_count;
 
-                    new_controller.gamepad.gamepad_data   = SDL_OpenGamepad(new_controller.gamepad.gamepad_id);
+                    new_controller.gamepad.gamepad_data   = SDL_OpenGamepad(new_controller.gamepad.gamepadID);
                     new_controller.gamepad.has_rumble     = SDL_RumbleGamepad(new_controller.gamepad.gamepad_data, 0x1, 0x1, 1);
                     new_controller.gamepad.stick_deadzone = INPUT_MANAGER_GAMEPAD_DEFAULT_DEADZONE;
 
@@ -620,6 +902,37 @@ main(void)
         }
         event_handler.event_count = 0;
 
+        // NOTE(Sleepster): (fake) UI LOOP 
+        input_controller_t *controller = event_handler.controllers + event_handler.active_controller_index;
+        for(s32 event_index = 0;
+            event_index < controller->event_count;
+            ++event_index)
+        {
+            input_event_t *event = controller->events + event_index;
+            if(!event->consumed)
+            {
+                switch(event->type)
+                {
+                    case INPUT_EVENT_TYPE_PRESSED:
+                    {
+                        printf("Simulated UI Event: '%c' %s...\n", event->inputID, "Pressed");
+                    }break;
+                    case INPUT_EVENT_TYPE_DOWN:
+                    {
+                        printf("Simulated UI Event: '%c' '%s'...\n", event->inputID, "Down");
+                    }break;
+                    case INPUT_EVENT_TYPE_RELEASED:
+                    {
+                        printf("Simulated UI Event: '%c' '%s'...\n", event->inputID, "Released");
+                    }break;
+                    case INPUT_EVENT_TYPE_AXIS_MOVED:
+                    {
+                        printf("Simulated UI Event: '%s'...\n", "Axis Motion");
+                    }break;
+                }
+            }
+        }
+
         // NOTE(Sleepster): Simulate 
         if(delta_time >= (gc->tick_rate * 2.0f))
         {
@@ -629,41 +942,43 @@ main(void)
         dt_accumulator += delta_time;
         while(dt_accumulator >= gc->tick_rate)
         {
-            input_controller_t *controller = event_handler.controllers + event_handler.active_controller_index;
-            for(s32 event_index = 0;
-                event_index < controller->event_count;
-                ++event_index)
+            // NOTE(Sleepster): Resolve the game_actions
+            for(game_action_t &action: event_handler.game_actions)
             {
-                input_event_t *event = controller->events + event_index;
-                if(!event->consumed)
+                action.axis2D_value = vec2_zero();
+                action.axis1D_value = 0.0f;
+                action.button_flags = ((action.button_flags & ~INPUT_MANAGER_ACTION_BUTTON_FLAG_PRESSED) | ((action.button_flags & ~INPUT_MANAGER_ACTION_BUTTON_FLAG_RELEASED)));
+                switch(action.action_binding_type)
                 {
-                    switch(event->type)
+                    case INPUT_MANAGER_GAME_ACTION_MAPPING_TYPE_BUTTON:
                     {
-                        case INPUT_EVENT_TYPE_PRESSED:
-                        {
-                            printf("Controller Event: '%c' %s...\n", event->inputID, "Pressed");
-                        }break;
-                        case INPUT_EVENT_TYPE_DOWN:
-                        {
-                            printf("Controller Event: '%c' '%s'...\n", event->inputID, "Down");
-                        }break;
-                        case INPUT_EVENT_TYPE_RELEASED:
-                        {
-                            printf("Controller Event: '%c' '%s'...\n", event->inputID, "Released");
-                        }break;
-                        case INPUT_EVENT_TYPE_AXIS_MOVED:
-                        {
-                            printf("Controller Event: '%s'...\n", "Axis Motion");
-                        }break;
-                    }
+                        s_im_game_action_process_button_state(controller, &action);
+                    }break;
+                    case INPUT_MANAGER_GAME_ACTION_MAPPING_TYPE_AXIS1D:
+                    {
+                        s_im_game_action_process_axis1D_state(controller, &action);
+                    }break;
+                    case INPUT_MANAGER_GAME_ACTION_MAPPING_TYPE_AXIS2D:
+                    {
+                        s_im_game_action_process_axis2D_state(controller, &action);
+                    }break;
                 }
-                event->consumed = true;
             }
-            controller->event_count = 0;
-                
+
+            if(movement_action->axis2D_value.x != 0 || movement_action->axis2D_value.y != 0)
+            {
+                printf("Axis value: '%.02f', '%.02f'...\n", movement_action->axis2D_value.x, movement_action->axis2D_value.y);
+            }
+
+            if(jump_action->button_flags & INPUT_MANAGER_ACTION_BUTTON_FLAG_PRESSED)
+            {
+                printf("jumped pressed\n");
+            }
+
             dt_accumulator -= gc->tick_rate;
         }
         // NOTE(Sleepster): Simulate 
+        controller->event_count = 0;
 
         SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
         SDL_RenderClear(renderer);
